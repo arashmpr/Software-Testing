39df665f8336f2652f7bd7d37ab6c6690cd6dbb2
arashmpr <arashmdp@gmail.com>
2023-12-23 20:17:59 +0330

    Unit tests for Commodity class is done

--- CA1/src/test/java/model/CommodityTest.java
+++ CA1/src/test/java/model/CommodityTest.java
@@ -1,42 +1,49 @@
 package model;
 
 import exceptions.NotInStock;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 class CommodityTest {
-
-    private Commodity commodity;
-
-    @BeforeEach
-    void setUp() {
-        commodity = new Commodity();
+    @Test
+    void testUpdateInStockHasStock() throws NotInStock {
+        Commodity commodity = new Commodity();
         commodity.setInStock(10);
-        commodity.setInitRate(4.5f);
-    }
 
-    @Test
-    void updateInStock_WithPositiveAmount_ShouldIncreaseInStock() throws NotInStock {
         int amount = 5;
-        commodity.updateInStock(amount);
         int expectedInStock = 10 + amount;
-        Assertions.assertEquals(expectedInStock, commodity.getInStock());
-    }
 
-    @Test
-    void updateInStock_WithNegativeAmount_ShouldDecreaseInStock() throws NotInStock {
-        int amount = -5;
         commodity.updateInStock(amount);
-        int expectedInStock = 10 + amount;
-        Assertions.assertEquals(expectedInStock, commodity.getInStock());
-    }
 
+        assertEquals(expectedInStock, commodity.getInStock());
+    }
     @Test
-    void updateInStock_WithNegativeAmountGreaterThanInStock_ShouldThrowNotInStockException() {
+    void testUpdateInStockInsufficient() {
+        Commodity commodity = new Commodity();
+        commodity.setInStock(10);
+
         int amount = -15;
-        Assertions.assertThrows(NotInStock.class, () -> commodity.updateInStock(amount));
+
+        assertThrows(NotInStock.class, () -> commodity.updateInStock(amount));
+    }
+
+    @Test
+    void testAddRate() {
+        Commodity commodity = new Commodity();
+        commodity.setInitRate(8);
+
+        commodity.addRate("user1", 5);
+        commodity.addRate("user2", 8);
+
+        Map<String, Integer> expectedUserRatings = new HashMap<>();
+        expectedUserRatings.put("user1", 5);
+        expectedUserRatings.put("user2", 8);
+        float expectedRating = 7;
+
+        assertEquals(expectedUserRatings, commodity.getUserRate());
+        assertEquals(expectedRating, commodity.getRating());
     }
 }
